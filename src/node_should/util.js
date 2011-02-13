18c60168d438f67eb65b0d0a3eb3bf190d5a259c
lukebayes <lbayes@patternpark.com>
2011-02-12 23:12:17 -0800

    Added comments to util functions

--- src/node_should/util.js
+++ src/node_should/util.js
@@ -1,5 +1,21 @@
 var fs = require("fs");
 
+/**
+ * Call callback for each file found in path with a name that matches the
+ * provided expression, and provides an options error, the file name and
+ * the file contents.
+ *
+ * Call to this method will usually look something like the following:
+ *
+ *     readEachFileMatching(/_test.js/, 'test/', function(err, file, content) {
+ *       if (err) throw err;
+ *       console.log('>> Found file: ' + file + ' with: ' + content);
+ *     });
+ *
+ * @param expr RegExp of the file names to look for.
+ * @param path Directory to scan (recursively) for files.
+ * @param callback Function that will be called with each matched file.
+ */
 var readEachFileMatching = function(expr, path, callback) {
   eachFileMatching(expr, path, function(err, file) {
     fs.readFile(file, function(err, content) {
@@ -9,6 +25,24 @@ var readEachFileMatching = function(expr, path, callback) {
   });
 }
 
+/**
+ * Calls callback for each file found in path with a name that matches the
+ * provided expression and provides an optional error and the file name.
+ *
+ * Calls to this method will usually look something like the following:
+ *
+ *     eachFileMatching(/_test.js/, 'test/', function(err, file) {
+ *       if (err) throw err;
+ *       console.log('>> Found file with: ' + file);
+ *     });
+ *
+ * If you're going to immediately read the contents of the file, go ahead
+ * and use readEachFileMatching.
+ *
+ * @param expr RegExp of the file names to look for.
+ * @param path Directory to scan (recursively) for files.
+ * @param callback Function that will be called with each matched file.
+ */
 var eachFileMatching = function(expr, path, callback) {
   var prependPathToFiles = function(dir, files) {
     return files.map(function(file) { 
--- test/unit/runner_test.js
+++ test/unit/runner_test.js
@@ -1,7 +1,6 @@
 
 require('../common');
 var assert = require('assert');
-var printer = require('fake_printer');
 var runFilesMatching = require('node_should/runner').runFilesMatching;
 
 assert.ok(runFilesMatching instanceof Function);
