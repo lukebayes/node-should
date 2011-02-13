841995b76700bb08799e8f449984f8d031ac6c8f
lukebayes <lbayes@patternpark.com>
2011-02-13 15:26:40 -0800

    Tests are executable (synchronously) with setups and teardowns

--- src/node_should/context.js
+++ src/node_should/context.js
@@ -1,4 +1,117 @@
 
-var Context = function() {
+var Composite = require('node_should/composite').Composite;
+var ArrayIterator = require('node_should/array_iterator').ArrayIterator;
+var util = require('util');
+
+var Context = function(label) {
+  Composite.call(this);
+  this._label = label || '';
+  this._setupHandlers = [];
+  this._teardownHandlers = [];
+  this._testHandlers = [];
 }
 
+util.inherits(Context, Composite);
+
+Context.prototype.addSetupHandler = function(handler) {
+  if (handler == null) throw 'The handler provided to Context.addSetupHandler must not be null';
+  this._setupHandlers.push(handler);
+}
+
+Context.prototype.addTeardownHandler = function(handler) {
+  if (handler == null) throw 'The handler provided to Context.addTeardownHandler must not be null';
+  this._teardownHandlers.push(handler);
+}
+
+Context.prototype.addTestHandler = function(handler) {
+  if (handler == null) throw 'The handler provided to Context.addTestHandler must not be null';
+  this._testHandlers.push(handler);
+}
+
+Context.prototype.execute = function(completeHandler) {
+  if (this._testHandlers.length == 0) throw 'Context.execute called with no tests.';
+  var setupHandlers = this._getSetupHandlers();
+  var testHandlers = this._testHandlers;
+  var teardownHandlers = this._teardownHandlers;
+
+  var testHandlerList = [];
+  var handlers = null;
+  // Build an array of arrays, each item is an array of all
+  // setup methods, a single test method, and all teardowns.
+  // Each of these collections of methods will be executed in
+  // a unique scope that is only shared by them.
+  testHandlers.forEach(function(handler) {
+    handlers = setupHandlers.slice(0);
+    handlers = handlers.concat([handler]);
+    handlers = handlers.concat(teardownHandlers.slice(0));
+    testHandlerList.push(new ArrayIterator(handlers));
+  });
+  
+  var testsToComplete = testHandlerList.length;
+  var testsCompleted = 0;
+  var perTestCompleteHandler = function() {
+    console.log(">> checking " + testsCompleted + " and : " + testsToComplete);
+    if (testsCompleted++ >= testsToComplete) {
+      completeHandler.call(this);
+    }
+  }
+
+  var self = this;
+  // Initiate all tests immediately:
+  var testIterator = new ArrayIterator(testHandlerList);
+  while (testIterator.hasNext()) {
+    var scope = {
+      pendingAsyncs: 0,
+      async: function(callback) {
+        scope.pendingAsyncs++;
+        
+        return function() {
+          scope.pendingAsyncs--;
+          callback.apply(scope, arguments);
+          if (scope.pendingAsyncs == 0) {
+            self._executeNextSetupTestOrTeardown(testIterator.next(), scope, perTestCompleteHandler);
+          }
+        }
+      }
+    }
+    this._executeNextSetupTestOrTeardown(testIterator.next(), scope, perTestCompleteHandler);
+  }
+}
+
+Context.prototype._executeNextSetupTestOrTeardown = function(iterator, scope, completeHandler) {
+  if (iterator.hasNext()) {
+    var handler = iterator.next();
+    handler.call(scope);
+    if (scope.pendingAsyncs == 0) {
+      this._executeNextSetupTestOrTeardown(iterator, scope);
+    }
+  }
+}
+
+Context.prototype._getSetupHandlers = function() {
+  return this._setupHandlers;
+}
+
+Context.prototype._getTeardownHandlers = function() {
+  return this._teardownHandlers;
+}
+
+Context.prototype.getLabel = function() {
+  var labelParts = [];
+  if (this.parent) {
+    var parentLabel = this.parent.getLabel();
+    if (parentLabel != '') {
+      labelParts.push(parentLabel);
+    }
+  }
+  if (this._label != '') {
+    labelParts.push(this._label);
+  }
+  return labelParts.join(' ');
+}
+
+Context.prototype.toString = function() {
+  return '[Context label=' + this.getLabel() + ']';
+}
+
+exports.Context = Context;
--- test/unit/context_test.js
+++ test/unit/context_test.js
@@ -1,3 +1,174 @@
 
+var Context = require('node_should/context').Context;
+var assert = require('assert');
 require('../common');
 
+/* Configuration */
+
+(function contextIsInstantiable() {
+  var c = new Context();
+})();
+
+(function contextIsComposite() {
+  var c = new Context();
+  c.addChild(new Context());
+  assert.equal(1, c.numChildren);
+})();
+
+(function contextAddSetupHandlerFailsWithNullHandler() {
+  var c = new Context();
+  assert.throws(function() {
+    c.addSetupHandler();
+  }, /must not be null/);
+})();
+
+(function contextAcceptsSetupHandler() {
+  var c = new Context();
+  c.addSetupHandler(function() {});
+})();
+
+(function contextAddTeardownHandlerFailsWithNullHandler() {
+  var c = new Context();
+  assert.throws(function() {
+    c.addTeardownHandler();
+  }, /must not be null/);
+})();
+
+(function contextAcceptsTeardownHandlers() {
+  var c = new Context();
+  c.addTeardownHandler(function() {});
+})();
+
+(function contextUsesEmptyStringAsDefaultLabel() {
+  var c = new Context();
+  assert.equal('', c.getLabel());
+})();
+
+(function contextFailsWithNullTestHandler() {
+  var c = new Context();
+  assert.throws(function() {
+    c.addTestHandler();
+  }, /must not be null/);
+})();
+
+(function contextAcceptsTestHandler() {
+  var c = new Context();
+  c.addTestHandler(function() {});
+})();
+
+(function contextAcceptsLabel() {
+  var c = new Context('root');
+  assert.equal('root', c.getLabel());
+})();
+
+(function contextBuildsLabelFromComposite() {
+  var root = new Context('SomeClass');
+  var child1 = new Context('with child');
+  root.addChild(child1);
+
+  assert.equal('SomeClass with child', child1.getLabel());
+})();
+
+(function contextBuildsLabelFromComposite() {
+  var root = new Context('SomeClass');
+  var child1 = new Context();
+  root.addChild(child1);
+
+  assert.equal('SomeClass', child1.getLabel());
+})();
+
+(function contextBuildsLabelFromComposite() {
+  var root = new Context();
+  var child1 = new Context('SomeClass');
+  root.addChild(child1);
+
+  assert.equal('SomeClass', child1.getLabel());
+})();
+
+/* Execution */
+
+(function contextFailsToExecuteIfNoTests() {
+  var c = new Context();
+  assert.throws(function() {
+    c.execute();
+  }, /no tests/);
+
+})();
+
+(function contextRunsSetupsThenTestsThenTeardowns() {
+  var executed = [];
+  var c = new Context('SomeClass');
+  c.addSetupHandler(function() {
+    executed.push('setup');
+  });
+  c.addTestHandler(function() {
+    executed.push('test');
+  });
+  c.addTeardownHandler(function() {
+    executed.push('teardown');
+  });
+  c.execute();
+
+  var expected = ['setup', 'test', 'teardown']; 
+  assert.equal(expected.length, executed.length);
+  assert.equal(expected[0], executed[0]);
+  assert.equal(expected[1], executed[1]);
+  assert.equal(expected[2], executed[2]);
+})();
+
+(function contextRunsHandlersInSharedScope() {
+  var c = new Context('SomeClass');
+  c.addSetupHandler(function() {
+    this.foo = 'bar';
+  });
+  c.addTestHandler(function() {
+    assert.equal(this.foo, 'bar');
+  });
+  c.execute();
+})();
+
+(function setupsCalledForEachTestMethod() {
+  var executed = [];
+  var c = new Context('SomeClass');
+  c.addSetupHandler(function() { executed.push('setup1'); });
+  c.addSetupHandler(function() { executed.push('setup2'); });
+  c.addTestHandler(function() { executed.push('test1') });
+  c.addTestHandler(function() { executed.push('test2') });
+  c.addTestHandler(function() { executed.push('test3') });
+  c.addTestHandler(function() { executed.push('test4') });
+  c.addTeardownHandler(function() { executed.push('teardown1'); });
+  c.addTeardownHandler(function() { executed.push('teardown2'); });
+  c.execute();
+
+  assert.equal(20, executed.length);
+})();
+
+(function testMethodsCalledWithDifferentScope() {
+  var firstScope = null;
+  var secondScope = null;
+  var c = new Context();
+  c.addTestHandler(function() { firstScope = this });
+  c.addTestHandler(function() { secondScope = this });
+  c.execute();
+  assert.notStrictEqual(firstScope, secondScope);
+})();
+
+/*
+(function contextRunsSetupAsynchronouslyIfRequested() {
+  var executed = [];
+  var c = new Context('SomeClass');
+  c.addSetupHandler(function() {
+    setTimeout(this.async(function() {
+      executed.push('asyncsetup');
+    }), 0);
+  });
+  c.addTestHandler(function() {
+    executed.push('test');
+  });
+  c.execute(function() {
+    assert.equal(2, executed.length);
+    assert.equal('asyncsetup', executed[0]);
+    assert.equal('test', executed[1]);
+  });
+})();
+*/
